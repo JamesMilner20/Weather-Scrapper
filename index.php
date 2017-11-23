0b8ec61597b68608c4cfc445227723c80520df02
jamesmilner20 <jamesduah20@gmail.com>
2017-11-23 22:40:58 +0000

    html, css done. PhP begins

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <title>Weather Scraper</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+
+      <div class="row">
+
+        <div class="col-md-6 offset-md-3 center">
+
+        <h1 class="center white">Weather Predictor</h1>
+
+        <p class="lead center white">Enter your city below to get a forcast</p>
+
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Paris, Accra...">
+
+          </div>
+          <button class="btn btn-success btn-lg" type="button" name="button">Find Forcast for City</button>
+
+        </form>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
